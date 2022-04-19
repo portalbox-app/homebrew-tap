26adceb48ec5bbdfffb72b9027315f6307f0be37
wooden-worm <wooden-worm@portalbox.app>
2022-04-18 22:31:09 -0700

    Links & hashes

--- Formula/portalbox.rb
+++ Formula/portalbox.rb
@@ -5,13 +5,21 @@ class Portalbox < Formula
   version "0.0.1"
   
   on_macos do
+    url "https://github.com/portalbox-app/portalbox/releases/download/v#{version}/portalbox-#{version}-macos-x64.tar.gz"
+    sha256 "1aa01d06ab1a030ae74ed6f8b998a739338b26366e7b2f9b0a4db3976f97dcaf"
   end
-  
+
   on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/portalbox-app/portalbox/releases/download/v#{version}/portalbox-#{version}-linux-x64.tar.gz"
+      sha256 "8c903c57beddf8c3d155ed656f26d8e066d9aa960b5bbd62f9d2624c2d172f2a"
+    end
   end
 
   def install
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install "client"
+    bin.install Dir["wwwroot"]
+    bin.install Dir["website"]
   end
 
   test do
