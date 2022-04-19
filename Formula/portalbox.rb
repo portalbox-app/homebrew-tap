10201e6f1d409b313f0d2e23fe43e0a909804380
wooden-worm <wooden-worm@portalbox.app>
2022-04-18 23:16:30 -0700

    Correct version + libexec install

--- Formula/portalbox.rb
+++ Formula/portalbox.rb
@@ -2,24 +2,23 @@ class Portalbox < Formula
   desc "PortalBox client"
   homepage "https://www.portalbox.app"
   license "MIT"
-  version "0.0.1"
+  version "0.1.0"
   
   on_macos do
     url "https://github.com/portalbox-app/portalbox/releases/download/v#{version}/portalbox-#{version}-macos-x64.tar.gz"
-    sha256 "1aa01d06ab1a030ae74ed6f8b998a739338b26366e7b2f9b0a4db3976f97dcaf"
+    sha256 "671b9117ec3ac65bd8bc9d992d8191c249d54ae1628691e7931766c24fc26af0"
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/portalbox-app/portalbox/releases/download/v#{version}/portalbox-#{version}-linux-x64.tar.gz"
-      sha256 "8c903c57beddf8c3d155ed656f26d8e066d9aa960b5bbd62f9d2624c2d172f2a"
+      sha256 "0371960515dc4a39c57b4cc85b5dc593bb349a9253d715e8dd79b1e3faa5280f"
     end
   end
 
   def install
-    bin.install "client"
-    bin.install Dir["wwwroot"]
-    bin.install Dir["website"]
+    libexec.install Dir["*"]
+    bin.write_exec_script libexec/"client"
   end
 
   test do
